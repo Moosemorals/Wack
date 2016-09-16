2e85458f9f8ad529336f52eb2dd9a21d6c1b4db7
Osric Wilkinson <osric@fluffypeople.com>
2016-09-16 22:24:26 +0100

    Now using CSS transitions. Smooth

--- src/main/webapp/Wack/wack.js
+++ src/main/webapp/Wack/wack.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * The MIT License
  *
  * Copyright 2016 osric.
@@ -22,7 +22,30 @@
  * THE SOFTWARE.
  */
 
-function getRandomInt() {  
+
+var g = {
+    score : 0,
+    time : 0,
+    lastTick : 0,
+    running : true
+};
+
+function showText(id, text) {
+    var s = document.getElementById(id);
+    
+    while (s.hasChildNodes()) {
+        s.removeChild(s.firstChild);
+    }
+    
+    s.appendChild(document.createTextNode(text));    
+}
+
+function score() {
+    g.score += 1;
+    showText("score", g.score);
+}
+
+function getRandomInt() {
   return Math.floor(Math.random() * 4);
 }
 
@@ -30,70 +53,113 @@ function drawGrid() {
     var i, j, row, cell;
     var grid = document.getElementById("grid");
     var parent = grid.parentNode;
-    
+
     parent.removeChild(grid);
-    
-    
+
+
     for (i = 0; i < 4; i += 1) {
         row = document.createElement("div");
         row.classList.add("row");
         for (j = 0; j < 4; j += 1) {
-            cell = document.createElement("div");            
+            cell = document.createElement("div");
             cell.id = "cell-" + i +"-" + j;
             cell.classList.add("cell");
-            row.appendChild(cell);            
-        }
-        grid.addEventListener("click", boxClick);
+            row.appendChild(cell);
+        }        
         grid.appendChild(row);
-    }        
-    
+    }
+
     parent.appendChild(grid);
+}
+
+function tidy(box) {
+    box.removeEventListener("click", boxClick);
+    box.removeEventListener("transitionend", timeout);
+    box.addEventListener("transitionend", function () {
+        box.parentNode.removeChild(box);
+        if (g.running) {
+            nextMole();
+        }
+    });
+    
 }
 
 function timeout(e) {
     var box = e.target;
-    hideBox(box);    
+    
+    tidy(box);
+    
+    box.classList.remove("popup");
+    box.classList.add("popdown");
+
+    box.style.height = 0;
 }
 
-function showBox(cell) {    
-    cell.addEventListener("click", boxClick);
-    cell.addEventListener("animationend", timeout);
+function boxClick(e) {
+    var box = e.target;
+
+    tidy(box);
     
+    box.style.height = box.scrollHeight + "px";
+
+    box.classList.remove("popup");
+    box.classList.add("popout");
+    
+    box.style.background = "#fff";
+    score();
+}
+
+function showBox(cell) {
     var box = document.createElement("div");
-    
-    box.classList.add("cell");
+
     box.classList.add("pop");
     box.classList.add("popup");
+    box.style.height = "0px";
     
-    cell.appendChild(box);  
-}
+    setTimeout(function () {
+        box.style.height = "50px";
+        box.style.background = "#ffcc66";
+    }, 25);
 
-function hideBox(box) {
-    box.removeEventListener("click", boxClick);
-    box.removeEventListener("animationend", timeout);
-    box.classList.add("popdown");
-    box.addEventListener("animationend", function () { box.parentNode.removeChild(box); });
-}
-
-function boxClick(e) {    
+    box.addEventListener("click", boxClick);
+    box.addEventListener("transitionend", timeout);
     
-    var box = e.target;    
-    hideBox(box);
+    cell.appendChild(box);
 }
 
-function runGame() {    
+function nextMole() {
     var row = getRandomInt();
     var col = getRandomInt();
-    
+
     var cell = document.getElementById("cell-" + row + "-" + col);
-    
+
     showBox(cell);
-    setTimeout(runGame, 2500);
+}
+
+function tick() {
+    var sleep;
+    var now = Date.now();
+    var diff = (now - g.lastTick);
+    g.lastTick = now;
+
+    g.time += diff;
+    
+    showText("time", 30 - (Math.floor(g.time / 1000) ));
+    
+    sleep = 1000 - (g.time % 1000);
+    
+    if (g.time < 30000) {
+        setTimeout(tick, sleep);
+    } else {
+        g.running = false;
+    }
 }
 
 function init() {
     drawGrid();
-    runGame();
+    g.lastTick = Date.now();
+    nextMole();
+    tick();
 }
 
 document.addEventListener("DOMContentLoaded", init);
